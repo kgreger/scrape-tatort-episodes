aaf6d76f5f81ec9b435675022d460f972f50adc4
Konstantin Greger <kgreger@tableau.com>
2017-01-10 11:55:23 +0100

    add geodata; add merge of episodes, investigators and geodata

--- scrape-tatort-episodes.R
+++ scrape-tatort-episodes.R
@@ -1,4 +1,7 @@
+#install.packages("dplyr")
 #install.packages("rvest")
+#install.packages("xlsx")
+library(dplyr)
 library(rvest)
 library(xlsx)
 setwd("~/GitHub/scrape-tatort-episodes/")
@@ -6,29 +9,61 @@ monate <- c("Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt
 dienstgrade <- c("Hauptkommissare", "Kommissare", "Hauptkommissar", "Kommissar", "Polizeipsychologin", 
                  "Oberinspektor", "Inspektor", "Kommissarin", "Zollfahnder", "Verdeckter Ermittler", 
                  "MAD-Oberstleutnant", "Polizeihauptmeister", "Wachtmeister")
+einsatzorte <- c("Hamburg", "Saarbrücken", "Köln", "Stuttgart", "Frankfurt", "Kiel", "Wien", "Berlin", 
+                 "München", "Baden-Baden", "Bremen", "Essen", "Hannover", "Mainz", "Braunschweig", 
+                 "Endwarden", "Duisburg", "Lübeck", "Bremerhaven", "Stade", "Heppenheim", "Ludwigshafen", 
+                 "Bern", "Leipzig", "Düsseldorf", "Konstanz", "Münster", "Wiesbaden", "Luzern", "Dortmund", 
+                 "Erfurt", "Weimar", "Nürnberg", "Dresden", "Freiburg")
+einsatzbundesländer <- c("Hamburg", "Saarland", "Nordrhein-Westfalen", "Baden-Württemberg", "Hessen", 
+                         "Schleswig-Holstein", "Wien", "Berlin", "Bayern", "Baden-Württemberg", "Bremen", 
+                         "Nordrhein-Westfalen", "Niedersachsen", "Rheinland-Pfalz", "Niedersachsen", 
+                         "Schleswig-Holstein", "Nordrhein-Westfalen", "Schleswig-Holstein", "Bremen", 
+                         "Niedersachsen", "Hessen", "Rheinland-Pfalz", "Bern", "Sachsen", "Nordrhein-Westfalen", 
+                         "Baden-Württemberg", "Niedersachsen", "Hessen", "Luzern", "Nordrhein-Westfalen", 
+                         "Thüringen", "Thüringen", "Bayern", "Sachsen", "Baden-Württemberg")
+einsatzländer <- c("Deutschland", "Deutschland", "Deutschland", "Deutschland", "Deutschland", "Deutschland", 
+                   "Österreich", "Deutschland", "Deutschland", "Deutschland", "Deutschland", "Deutschland", 
+                   "Deutschland", "Deutschland", "Deutschland", "Deutschland", "Deutschland", "Deutschland", 
+                   "Deutschland", "Deutschland", "Deutschland", "Deutschland", "Schweiz", "Deutschland", 
+                   "Deutschland", "Deutschland", "Deutschland", "Deutschland", "Schweiz", "Deutschland", 
+                   "Deutschland", "Deutschland", "Deutschland", "Deutschland", "Deutschland")
+Geodaten <- data.frame(cbind(iconv(einsatzorte, "LATIN1", "UTF-8"), 
+                             einsatzbundesländer, 
+                             einsatzländer), 
+                       stringsAsFactors = FALSE)
 
 
-# Folgen
-## scrape data from Wikipedia page
+# Tatorte
+## Daten von der Wikipedia-Seite parsen
 url <- "https://de.wikipedia.org/wiki/Liste_der_Tatort-Folgen"
-tatort <- url %>%
+tatorte <- url %>%
   read_html() %>%
   html_node(xpath='//*[@id="mw-content-text"]/table[1]') %>%
-  html_table()
-
-## fix data using regex
-### Titel
-tatort$Titel <- gsub("\\(Folge .+? trägt den(?: gleichen|selben) Titel\\)", 
-                     "", 
-                     tatort$Titel)
-### Sender
-tatort$Sender <- gsub("\\n", 
+  html_table() %>% 
+  ## Daten mittels RegEx verarbeiten
+  ### Folge
+  mutate(unechter.Tatort = grepl("\\d(?:a|b)\\*", Folge)) %>% 
+  filter(!unechter.Tatort) %>% 
+  ### Titel
+  mutate(Titel = gsub("\\(Folge .+? trägt den(?: gleichen|selben) Titel\\)", 
                       "", 
-                      tatort$Sender)
+                      Titel)) %>% 
+  ### Sender
+  mutate(Sender = gsub("\\n", 
+                       "", 
+                       Sender)) %>% 
+  ### Besonderheiten
+  mutate(Besonderheiten = gsub("\\[.+?\\]", 
+                               "", 
+                               Besonderheiten)) %>% 
+  ### Fall
+  mutate(Fall = gsub("\\s.*", 
+                     "", 
+                     Fall))
 ### Erstausstrahlung
 Erstausstrahlung.Datum <- sub("(\\d{1,2})\\.\\s?(\\w+?)\\.?\\s?(\\d{4})(.*)", 
                                "\\1@\\2@\\3@", 
-                               tatort$Erstausstrahlung, 
+                               tatorte$Erstausstrahlung, 
                                perl = TRUE) %>% 
   strsplit("@")
 Erstausstrahlung.Monat <- sapply(Erstausstrahlung.Datum, 
@@ -36,54 +71,280 @@ Erstausstrahlung.Monat <- sapply(Erstausstrahlung.Datum,
                                  2) %>% 
   substr(1, 3) %>% 
   match(., monate)
-tatort$Erstausstrahlung <- paste(sapply(Erstausstrahlung.Datum, 
-                                        "[",
-                                        1), 
-                                 Erstausstrahlung.Monat, 
-                                 sapply(Erstausstrahlung.Datum, 
-                                        "[",
-                                        3), 
-                                 sep = ".")
+tatorte$Erstausstrahlung <- paste(sapply(Erstausstrahlung.Datum, 
+                                         "[",
+                                         1), 
+                                  Erstausstrahlung.Monat, 
+                                  sapply(Erstausstrahlung.Datum, 
+                                         "[",
+                                         3), 
+                                  sep = ".")
 ### Ermittler (komplett)
-tatort$Ermittler.komplett <- gsub("(\\n.*|\\(.*\\))", 
-                                  "", 
-                                  tatort$Ermittler)
+tatorte$Ermittler.komplett <- gsub("(\\n.*|\\(.*\\))", 
+                                   "", 
+                                   tatorte$Ermittler)
 ### Ermittler
-tatort$Ermittler <- gsub("\\s/.*", 
-                         "", 
-                         tatort$Ermittler.komplett)
-### Besonderheiten
-tatort$Besonderheiten <- gsub("\\[.+?\\]", 
-                         "", 
-                         tatort$Besonderheiten)
+tatorte$Ermittler <- gsub("\\s/.*", 
+                          "", 
+                          tatorte$Ermittler.komplett)
+tatorte <- tatorte %>% 
+  mutate(Ermittler = gsub("\\s/.*", 
+                          "", 
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub(" +$", 
+                          "", 
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Bienzle und Gächter$",
+                          "Bienzle",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Blum$",
+                          "Blum und Perlmann",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Blum und Perlmann, Matteo Lüthi$",
+                          "Blum und Perlmann",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Blum und Perlmann, Reto Flückiger$",
+                          "Blum und Perlmann",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Borowski und Brandt$",
+                          "Borowski",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Borowski und Jung$",
+                          "Borowski",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Carlucci und Gertsch$",
+                          "Carlucci",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Delius a.D.$",
+                          "Delius",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Dellwo$",
+                          "Dellwo und Sänger",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Sänger und Dellwo$",
+                          "Dellwo und Sänger",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Eisner$",
+                          "Eisner und Fellner",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Falke und Grosz$",
+                          "Falke",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Falke und Lorenz$",
+                          "Falke",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Flemming und Koch$",
+                          "Flemming",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Flemming, Koch und Ballauf$",
+                          "Flemming",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Flückiger und Lanning$",
+                          "Flückiger und Ritschard",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Hellmann und Ritter$",
+                          "Hellmann",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Lürsen$",
+                          "Lürsen und Stedefreund",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Lutz und Schreitle$",
+                          "Lutz",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Marek a. D.$",
+                          "Marek",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Odenthal$",
+                          "Odenthal und Kopper",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Ritter und Stark$",
+                          "Ritter",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Sänger$",
+                          "Dellwo und Sänger",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Sieland, Gorniak, Mohr und Schnabel$",
+                          "Sieland, Gorniak und Schnabel",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Stark$",
+                          "Ritter",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Steier und Mey$",
+                          "Steier",
+                          Ermittler)) %>% 
+  mutate(Ermittler = gsub("^Stoever$",
+                          "Stoever und Brockmöller",
+                          Ermittler))
+
+## Daten exportieren
+# write.csv(tatorte, 
+#           file = "Tatorte.csv", 
+#           row.names = FALSE)
 
 
-# Kommissare
-## scrape data from ARD page
+# ehemalige Kommissare
+## Daten von der ARD-Seite parsen
 url <- "http://www.daserste.de/unterhaltung/krimi/tatort/kommissare/kommissare-ausser-dienst-100.html"
 kommissare.ehemalig <- url %>%
   read_html() %>%
   html_node(xpath='//*[@id="content"]/div/div[2]/div[1]/div/div/div/div/div[3]/table') %>%
-  html_table()
-names(kommissare.ehemalig)[names(kommissare.ehemalig)=="Die Kommissare"] <- "Kommissare"
-kommissare.ehemalig <- kommissare.ehemalig[kommissare.ehemalig$Kommissare != "", -2]
+  html_table() %>% 
+  ## Spalten umbenennen
+  rename(Kommissare = `Die Kommissare`) %>% 
+  ## unnötige Spalte 2 entfernen
+  select(-2) %>% 
+  ## leere Zeilen entfernen
+  filter(Kommissare != "") %>% 
+  ## Daten mittels RegEx verarbeiten
+  ### Kommissare
+  mutate(Kommissare = gsub(paste(dienstgrade, 
+                                 collapse = "|"), 
+                           "", 
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("\\s{2,}", 
+                           " ", 
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^ +", 
+                           "", 
+                           Kommissare)) %>% 
+  ### Einsatzort
+  mutate(Einsatzort = gsub("^nordd. Kleinstadt$", 
+                           "Endwarden", 
+                           Einsatzort)) %>% 
+  ### fehlenden Kommissar hinzufügen
+  rbind(c("Felber", "Frankfurt", "Heinz Schubert", "HR", 1))
 
-## fix data using regex
-### Kommissare
-kommissare.ehemalig$Kommissare <- gsub(paste(dienstgrade, 
-                                             collapse = "|"), 
-                                       "", 
-                                       kommissare.ehemalig$Kommissare) %>% 
-  gsub("\\s{2,}", 
-       " ", 
-       .) %>% 
-  gsub("^ +", 
-       "", 
-       .)
+
+# aktuelle Kommissare
+## Daten von der ARD-Seite parsen
+url <- "http://www.daserste.de/unterhaltung/krimi/tatort/kommissare/tatort-filter-aktuelle-kommissare-100.html"
+kommissare.aktuell <- url %>%
+  read_html() %>%
+  html_nodes(css = '.headline') %>% 
+  sapply(function(x){html_text(html_children(x))}) %>% 
+  unlist() %>% 
+  matrix(ncol = 2, 
+         byrow = TRUE) %>% 
+  data.frame(stringsAsFactors = FALSE) %>% 
+  ## Spalten umbenennen
+  rename(Kommissare = X1, 
+         Einsatzort = X2) %>% 
+  ## Daten mittels RegEx verarbeiten
+  mutate(Einsatzort = gsub("\\(|\\)", "", Einsatzort))
 
 
-# write data to csv file
-write.xlsx(tatort, 
+# Zusammenführen ehemalige & aktuelle Kommissare
+kommissare <- kommissare.ehemalig %>% 
+  select(Kommissare, Einsatzort) %>% 
+  union(kommissare.aktuell) %>% 
+  ## Daten mittels RegEx verarbeiten
+  ### Einsatzort
+  mutate(Einsatzort = gsub("Franken",
+                           "Nürnberg",
+                           Einsatzort)) %>%
+  mutate(Einsatzort = gsub("Leipzig und Dresden",
+                           "Leipzig",
+                           Einsatzort)) %>%
+  ### Kommissare
+## add Felber (ep.303)
+  mutate(Kommissare = gsub("^Batic und Leitmayr mit Team$",
+                           "Batic und Leitmayr",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Borowski und Brandt$",
+                          "Borowski",
+                          Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Borowski und Jung$",
+                          "Borowski",
+                          Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Cenk Batu$",
+                           "Batu",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Heinz Brammer$",
+                           "Brammer",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Marianne Buchmüller$",
+                           "Buchmüller",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Casstorff und Oberkommissar Holicek$",
+                           "Casstorff und Holicek",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Sänger und Dellwo$",
+                           "Dellwo und Sänger",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Falke und Grosz$",
+                           "Falke",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Haferkamp$",
+                           "Haferkamp und Kreutzer",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Robert Hellmann$",
+                           "Hellmann",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Paul Kant und Jakob Varasani$",
+                           "Kant und Varanasi",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Ludwig Lenz$",
+                           "Lenz",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Dorn und Lessing$",
+                           "Lessing und Dorn",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Liersdahl$",
+                           "Liersdahl und Schäfermann",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Franz Markowitz$",
+                           "Markowitz",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Pfeiffer$",
+                           "Pfeifer",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Eva Saalfeld und Andreas Keppler$",
+                           "Saalfeld und Keppler",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Schimanski$",
+                           "Schimanski und Thanner",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Martin Schmidt$",
+                           "Schmidt",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Lea Sommer$",
+                           "Sommer",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Frank Steier$",
+                           "Steier",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Philipp von Burg und Gertsch$",
+                           "von Burg und Gertsch",
+                           Kommissare)) %>% 
+  mutate(Kommissare = gsub("^Till Ritter$",
+                           "Ritter",
+                           Kommissare)) 
+  
+
+## Daten exportieren
+# write.csv(kommissare, 
+#           file = "Kommissare.csv", 
+#           row.names = FALSE)
+
+
+# Zusammenführen Tatorte, Kommissare und Geodaten
+komplett <- tatorte %>% 
+  full_join(kommissare, 
+            by = c("Ermittler" = "Kommissare")) %>% 
+  filter(!is.na(Folge)) %>% 
+  select(Folge, Titel, Ermittler, Fall, Einsatzort) %>% 
+  left_join(Geodaten, 
+            by = c("Einsatzort" = "V1")) %>% 
+  rename(Stadt = Einsatzort, 
+         Bundesland = einsatzbundesländer, 
+         Land = einsatzländer) %>% 
+  transform(Folge = as.numeric(Folge), 
+            Fall = as.numeric(Fall)) %>% 
+  arrange(Folge)
+
+
+# write data to xslx file
+write.xlsx(komplett, 
            file = "Folgen.xlsx",
            sheetName = "Tatort", 
            row.names = FALSE)
